e8d7b97ab981cc5f106aa27b9ea8599810416dc5
gabriel.alegre <alegregabriel.90@gmail.com>
2021-09-01 20:08:08 -0300

    Inclui ejemplo de js

---
+++ ejemplo.js
@@ -0,0 +1,8 @@
+var nombre= prompt ("ingresa tu nombre");
+var apellido= prompt ("ingresa tu apellido");
+var naciemiento= parseInt(prompt("en que año naciste"));
+var edad = 2021 - naciemiento;
+alert ("Bienvenido " + nombre + " " + apellido + " tienes " + edad) 
+
+
+
